58e616c21da64e991c078e3c0623f3f11e03ccb6
adildotes <adildotes@gmail.com>
2024-12-03 14:11:42 +0500

    Removed unnecessary imports to clean up the code and improve readability.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-SplashScreen.preventAutoHideAsync();
 
-const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
